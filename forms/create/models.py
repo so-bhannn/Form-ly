a2a9730c1ef657da8527f7e2a99943489d596a20
Sobhan Ghosh <sobhanghosh.jsr@gmail.com>
2025-04-02 03:11:38 +0530

    create models for forms

--- forms/create/models.py
+++ forms/create/models.py
@@ -1,3 +1,81 @@
 from django.db import models
+from forms import settings
+import uuid
 
 # Create your models here.
+
+class Form(models.Model):
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='forms')
+    
+    title = models.CharField(max_length=100, blank=False)
+    description = models.TextField(blank=True)
+    form_id = models.UUIDField(unique=True, default=uuid.uuid4, editable=False, primary_key=True)
+
+
+    REQUIRED_FIELDS = ['owner', 'title']
+
+class QuestionType(models.TextChoices):
+    SHORT_ANSWER= 'SA', 'Short Answer'
+    PARAGRAPH= 'PA', 'Paragraph'
+    MULTIPLE_CHOICE= 'MC', 'Multiple Choice'
+    DROPDOWN= 'DD', 'Dropdown'
+    CHECKBOX= 'CB', 'Checkbox'
+
+class Question(models.Model):
+    form = models.ForeignKey(
+        Form,
+        on_delete=models.CASCADE,
+        related_name='questions'
+    )
+    text = models.CharField( max_length=200)
+    description = models.TextField(blank=True)
+    question_type= models.CharField(
+        max_length=2,
+        choices=QuestionType.choices,
+        default=QuestionType.SHORT_ANSWER
+    )
+    is_required= models.BooleanField(default=False)
+    order = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        ordering = ['order', 'id']
+
+class Option(models.Model):
+    question = models.ForeignKey(
+        Question,
+        on_delete=models.CASCADE,
+        related_name='options'
+    )
+    text = models.CharField(max_length=30)
+
+class Response(models.Model):
+    respondent = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete = models.SET_NULL,
+        null=True, blank=True
+    )
+
+    form = models.ForeignKey(
+        Form,
+        on_delete=models.CASCADE,
+        related_name='responses'
+    )
+
+    submitted_at = models.DateTimeField(auto_now_add=True, editable=False)
+
+class Answer(models.Model):
+    question = models.ForeignKey(
+        Question,
+        on_delete=models.CASCADE,
+        related_name='answers'
+    )
+    response = models.ForeignKey(
+        Response,
+        on_delete=models.CASCADE,
+        related_name='answers'
+    )
+
+    value = models.JSONField()
--- forms/create/views.py
+++ forms/create/views.py
@@ -1,19 +1,2 @@
-import json
-from django.shortcuts import render
-from django.http import JsonResponse,HttpResponseBadRequest
+from rest_framework.response import Response
 
-# Create your views here.
-
-def index(req):
-    return render(req,'create/index.html')
-
-def create_form(req):
-    if req.method == 'POST':
-        try:
-            data=json.loads(req.body)
-            print(data)
-        except json.JSONDecodeError:
-            return HttpResponseBadRequest('Invalid data')
-    else:
-        data={'error':'Invalid request method. Use GET.'}
-        return render(req,'create/create.html',{'data':data})
