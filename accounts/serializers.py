e4cf74c76d75a012d38623e5442c6216f2cd7058
Sobhan Ghosh <sobhanghosh.jsr@gmail.com>
2025-04-06 02:28:45 +0530

    feat accounts/serializers.py: add registerserializer

---
+++ accounts/serializers.py
@@ -0,0 +1,42 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User= get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=User
+        fields=('username','email')
+    
+class RegisterSerializer(serializers.ModelSerializer):
+    password=serializers.CharField(
+        write_only=True,
+        required=True,
+    )
+    
+    password2=serializers.CharField(
+        write_only=True,
+        required=True,
+    )
+
+    email= serializers.EmailField(required=True)
+
+    class Meta:
+        model=User
+        fields=('username','email','password','password2')
+
+    def validate_email(self,email):
+        if User.objects.filter(email__iexact=email).exists():
+            raise serializers.ValidationError("An account with the same email exists.")
+        return email
+    
+    def validate(self, attrs):
+        if attrs['password']!=attrs['password2']:
+            raise serializers.ValidationError("Both the passwords must match.")
+        return attrs
+    
+    def create(self, validated_data):
+        validated_data.pop('password2')
+
+        user=User.objects.create(**validated_data)
+        return user
