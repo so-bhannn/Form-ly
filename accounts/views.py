6d629edf6b513bf655a57038ec89b73120c0b9b2
Sobhan Ghosh <sobhanghosh.jsr@gmail.com>
2025-04-06 19:35:07 +0530

    feat accounts/serializers.py: add view for registering user

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import permissions,status
+from rest_framework.response import Response
+from rest_framework.generics import GenericAPIView
+from rest_framework.authtoken.models import Token
 
-# Create your views here.
+from .serializers import RegisterSerializer,UserSerializer
+
+class RegisterView(GenericAPIView):
+    serializer_class=RegisterSerializer
+    permission_classes=[permissions.AllowAny]
+
+    def post(self,request,*args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+
+        token,created = Token.objects.get_or_create(user=user)
+
+        return Response({
+            'user':UserSerializer(user, context=self.get_serializer_context()).data,
+            'token': token.key,
+            'message': 'User registered successfully'
+        },status=status.HTTP_201_CREATED)
