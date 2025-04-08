8b1b263bf6b63f415bff706dd1c6effe2c40130f
Sobhan Ghosh <sobhanghosh.jsr@gmail.com>
2025-04-08 12:56:12 +0530

    feat (accounts/views.py): add view for login

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.generics import GenericAPIView
 from rest_framework.authtoken.models import Token
 
-from .serializers import RegisterSerializer,UserSerializer
+from .serializers import RegisterSerializer,UserSerializer,LoginSerializer
 
 class RegisterView(GenericAPIView):
     serializer_class=RegisterSerializer
@@ -21,3 +21,21 @@ class RegisterView(GenericAPIView):
             'token': token.key,
             'message': 'User registered successfully'
         },status=status.HTTP_201_CREATED)
+    
+class LoginView(GenericAPIView):
+    serializer_class=LoginSerializer
+    permission_classes=[permissions.AllowAny]
+
+    def post(self,request,*args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+
+        token,_=Token.objects.get_or_create(user=user)
+
+        if user:
+            return Response({
+                'user': UserSerializer(user, context=self.get_serializer_context()).data,
+                'token': token.key,
+                'message': 'Logged in successfully'
+            },status=status.HTTP_200_OK)
