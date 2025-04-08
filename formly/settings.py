a19ff10d1a4a0c11fa77994f94a45eea05ba702d
Sobhan Ghosh <sobhanghosh.jsr@gmail.com>
2025-04-08 13:14:54 +0530

    fix (settings.py): add email backend to settings

--- formly/settings.py
+++ formly/settings.py
@@ -106,6 +106,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTHENTICATION_BACKENDS=[
+    'django.contrib.auth.backends.ModelBackend',
+    'accounts.backends.EmailBackend',
+]
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
