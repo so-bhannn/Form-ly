1b2dc02d5538948af3ec5a615213202540364e50
Sobhan Ghosh <sobhanghosh.jsr@gmail.com>
2025-04-11 01:12:11 +0530

    feat: update button component to include icons

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,10 +1,11 @@
 export default function Button ({
     content,
+    icon=null,
     black=true,
 }){
     return(
-        <button className={`rounded-lg text-sm font-geist-button text-black/90 border-1 px-4 py-2 hover:cursor-pointer ${black ? 'border-0 bg-black text-white hover:bg-black/80' : 'border-gray-300 hover:bg-gray-100'}`}>
-            {content}
+        <button className={`inline-flex justify-center items-center rounded-lg text-sm font-geist-button text-black/90 border-1 px-4 py-2 hover:cursor-pointer ${black ? 'border-0 bg-black text-white hover:bg-black/80' : 'border-gray-300 hover:bg-gray-100'}`}>
+            {icon && <i class={`${icon} text-xl pr-1.5`}></i>}{content}
         </button>
     )
 }
