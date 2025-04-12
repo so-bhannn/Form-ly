d319f4bc0b10bb2d19294c95b3059cd767b2dcdb
Sobhan Ghosh <sobhanghosh.jsr@gmail.com>
2025-04-13 01:49:33 +0530

    feat: add easy import for CustomSelect

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,9 +1,11 @@
 import Button from "./Button.jsx"
 import FormCard from "./FormCard.jsx"
 import Navbar from "./Navbar.jsx"
+import CustomSelect from "./CustomSelect.jsx"
 
 export {
     Button,
     FormCard,
     Navbar,
+    CustomSelect,
 }
