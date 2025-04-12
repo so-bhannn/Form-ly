53535e61970895140798ee1345b9dd57aba2a1d1
Sobhan Ghosh <sobhanghosh.jsr@gmail.com>
2025-04-13 01:42:24 +0530

    fix: changed class to className

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar=()=>{
     return(
         <div className="flex w-full min-h-16 justify-between items-center px-15 border-b-1 border-gray-200">
             <a href="/" className="flex gap-1 text-black/80">
-                <i class='bx bx-file text-3xl' undefined></i>
+                <i className='bx bx-file text-3xl'></i>
                 <div className="font-bricolage font-bold text-2xl">Formly</div>
             </a>
             {pathname==='/' &&
@@ -29,7 +29,7 @@ const Navbar=()=>{
             }
             {!(pathname === '/') &&
                 <a href="/user" className="flex gap-1">
-                <i class='bx bxs-user-circle text-3xl'></i>
+                <i className='bx bxs-user-circle text-3xl'></i>
             </a>}
         </div>
     )
