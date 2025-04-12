16781798d02662468ba6668a10a1a3eeda2ad383
Sobhan Ghosh <sobhanghosh.jsr@gmail.com>
2025-04-13 01:55:44 +0530

    configure routes for Home, Dashboard and Forms pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,25 @@
 import './App.css'
+import {
+  Home,
+  Dashboard,
+  AllForms
+} from './pages'
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/dashboard' element={<Dashboard/>} />
+          <Route path='/forms' element={<AllForms/>} />
+        </Routes>
+      </div>
+    </Router>
     </>
   )
 }
