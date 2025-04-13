be8068695251513e2cde98600218144999bf35f3
Sobhan Ghosh <sobhanghosh.jsr@gmail.com>
2025-04-13 21:30:36 +0530

    fix: minor changes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,17 +7,17 @@ const Navbar=()=>{
     const pathname = location.pathname;
 
     return(
-        <div className="flex w-full min-h-16 justify-between items-center px-15  bg-white border-b-1 border-gray-200 ">
+        <div className="flex w-full min-h-16 justify-between items-center px-5 bg-white/60 backdrop-blur-md border-b-1 border-gray-200 ">
             <a href="/" className="flex gap-1 text-black/80">
                 <i className='bx bx-file text-3xl'></i>
                 <div className="font-bricolage font-bold text-2xl">Formly</div>
             </a>
             {pathname==='/' &&
-                <div className="flex justify-between gap-6">
-                    <a href="/" className={`font-medium text-black/60 text-sm ${pathname === '/' ? 'text-black/90' : '' } hover:text-black/80 hover:cursor-pointer`}>Home </a>
-                    <a href="/dashboard" className={`font-medium text-black/60 text-sm ${pathname === '/dashboard' ? 'text-black/80' : '' } hover:text-black/80 hover:cursor-pointer`}>Dashboard</a>
-                    <a href="/pricing" className={`font-medium text-black/60 text-sm ${pathname === '/pricing' ? 'text-black/80' : '' } hover:text-black/80 hover:cursor-pointer`}>Pricing</a>
-                    <a href="/about" className={`font-medium text-black/60 text-sm ${pathname === '/about' ? 'text-black/80' : '' } hover:text-black/80 hover:cursor-pointer`}>About</a>
+                <div className="md:flex justify-between gap-6 hidden">
+                    <a href="/" className={`font-medium text-black/60 text-sm ${pathname === '/' ? 'text-black/90 underline-offset-8 underline' : '' } hover:text-black/80 hover:cursor-pointer`}>Home </a>
+                    <a href="/dashboard" className={`font-medium text-black/60 text-sm ${pathname === '/dashboard' ? 'text-black/80  underline-offset-8 underline' : '' } hover:text-black/80 hover:cursor-pointer`}>Dashboard</a>
+                    <a href="/pricing" className={`font-medium text-black/60 text-sm ${pathname === '/pricing' ? 'text-black/80 underline-offset-8 underline' : '' } hover:text-black/80 hover:cursor-pointer`}>Pricing</a>
+                    <a href="/about" className={`font-medium text-black/60 text-sm ${pathname === '/about' ? 'text-black/80 underline-offset-8 underline' : '' } hover:text-black/80 hover:cursor-pointer`}>About</a>
                 </div>
             }
             {pathname === '/' &&
