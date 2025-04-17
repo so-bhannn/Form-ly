08cc7a6798d339d8a0c84cc148c97c90466cd0a4
Sobhan Ghosh <sobhanghosh.jsr@gmail.com>
2025-04-17 23:47:56 +0530

    minor fix

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -2,10 +2,13 @@ export default function Button ({
     content,
     icon=null,
     black=true,
-    
+    scale=100,
+    onClick,
 }){
     return(
-        <button className={`inline-flex justify-center items-center rounded-lg text-sm font-geist-button text-black/90 border-1 px-4 py-2 hover:cursor-pointer ${black ? 'border-0 bg-black text-white hover:bg-black/80' : 'border-gray-300 hover:bg-gray-100'}`}>
+        <button
+        onClick={onClick?onClick:()=>{}}
+        className={`inline-flex justify-center items-center rounded-lg text-sm font-geist-button text-black/90 border-1 px-4 py-2 hover:cursor-pointer ${black ? 'border-0 bg-black text-white hover:bg-black/80' : 'border-gray-300 hover:bg-gray-100'} ${scale?`scale-${scale}`:''}`}>
             {icon && <i className={`${icon} text-xl pr-1.5`}></i>}{content}
         </button>
     )
