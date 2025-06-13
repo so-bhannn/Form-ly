4dce57ab7750559707152c60833798b131419f6a
Sobhan Ghosh <sobhanghosh.jsr@gmail.com>
2025-06-13 20:15:17 +0530

    feat: create custom user models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 
+from decouple import config
+import uuid
 # Create your models here.
+
+AUTH_PROVIDER = (
+    ('email','EMAIL'),
+    ('google','GOOGLE')
+)
+
+class CustomUserManager(BaseUserManager):
+
+    def create_user(self, email, auth_provider='email', password=None, *args, **kwargs):
+        if not email:
+            raise ValueError('Email is required.')
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, auth_provider=auth_provider, *args, **kwargs)
+
+        if not password:
+            raise ValueError("Password is required for email authentication.")
+        
+        user.set_password(password)
+
+        user.save(using=self._db)
+        return user
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+
+    id = models.CharField(max_length=10, unique=True, primary_key=True)
+
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    avatar = models.URLField(default=config('USER_PFP'))
+
+    auth_provider = models.CharField(max_length=20, choices= AUTH_PROVIDER, default= 'email')
+
+    is_active = models.BooleanField(default=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+
+    def save(self,*args, **kwargs):
+        if not self.id:
+            self.id= f'{uuid.uuid4().hex[:8].lower()}'
+        super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} ({self.email}) ({self.auth_provider})'
