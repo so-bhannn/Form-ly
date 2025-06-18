d3765efec86e7b7643764a093929925c00360536
Sobhan Ghosh <sobhanghosh.jsr@gmail.com>
2025-06-18 12:22:52 +0530

    chore: minor fix

--- accounts/backends.py
+++ accounts/backends.py
@@ -6,7 +6,7 @@ User=get_user_model()
 class EmailBackend(ModelBackend):
     def authenticate(self, request, username = None, password = None, **kwargs):
         try:
-            user=User.objects.get(email__iexact=username)
+            user=User.objects.get(email=username)
         except User.DoesNotExist:
             return None
         
