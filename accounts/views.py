ef279f4c46b06eccccc232da38ab51eb40700cc9
Sobhan Ghosh <sobhanghosh.jsr@gmail.com>
2025-07-22 09:37:37 +0530

    refactor: enhance authentication views to set refresh token in cookies; improve response structure and error handling

--- accounts/views.py
+++ accounts/views.py
@@ -1,18 +1,17 @@
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, get_user_model
 from rest_framework import permissions,status, exceptions
 from rest_framework.response import Response
 from rest_framework.generics import GenericAPIView
 from rest_framework_simplejwt.views import TokenRefreshView
 from rest_framework_simplejwt.exceptions import InvalidToken
 
-from .models import RefreshToken
 from .serializers import UserSerializer, RegisterSerializer,LoginSerializer,GoogleOAuthSerializer
 from .tokens import get_tokens_for_user
+from formly import settings
 class RegisterView(GenericAPIView):
     serializer_class=RegisterSerializer
     permission_classes=[permissions.AllowAny]
     http_method_names=['post']
-
     def post(self,request,*args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -20,14 +19,21 @@ class RegisterView(GenericAPIView):
 
         refresh_token, access_token = get_tokens_for_user(user)
 
-        RefreshToken.objects.create(user=user, token= refresh_token, )
-
-        return Response({
-            'user': UserSerializer(user).data,
-            'refresh': refresh_token,
+        response= Response({
+            'user':UserSerializer(user).data,
             'access': access_token,
-            'message': 'User registered successfully'
-        },status=status.HTTP_201_CREATED)
+            'message': 'User Registered successfully.'
+        },status=status.HTTP_200_OK)
+        response.set_cookie(
+            key='refresh',
+            value=refresh_token,
+            httponly=True,
+            max_age=settings.REFRESH_TOKEN_MAX_AGE,
+            secure=False,
+            samesite='None',
+            domain=None
+        )
+        return response
 
 class LoginView(GenericAPIView):
     serializer_class=LoginSerializer
@@ -43,12 +49,21 @@ class LoginView(GenericAPIView):
 
         refresh_token, access_token = get_tokens_for_user(user)
 
-        return Response({
+        response= Response({
             'user':UserSerializer(user).data,
-            'refresh': refresh_token,
             'access': access_token,
-            'message': 'User Logged in successfully'
+            'message': 'User logged in successfully.'
         },status=status.HTTP_200_OK)
+        response.set_cookie(
+            key='refresh',
+            value=refresh_token,
+            httponly=True,
+            max_age=settings.REFRESH_TOKEN_MAX_AGE,
+            secure=False,
+            samesite='None',
+            domain=None
+        )
+        return response
 
 class GoogleSignInView(GenericAPIView):
 
@@ -65,18 +80,37 @@ class GoogleSignInView(GenericAPIView):
         
         refresh_token, access_token = get_tokens_for_user(user)
 
-        return Response({
+        response= Response({
             'user':UserSerializer(user).data,
-            'refresh': refresh_token,
             'access': access_token,
-            'message': msg
         },status=status.HTTP_200_OK)
-    
+        response.set_cookie(
+            key='refresh',
+            value=refresh_token,
+            httponly=True,
+            max_age=settings.REFRESH_TOKEN_MAX_AGE,
+            secure=False,
+            samesite='None',
+            domain=None
+        )
+        return response
+
 class CustomTokenRefreshView(TokenRefreshView):
+    permission_classes=[permissions.AllowAny]
     def post(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
+        refresh_token= request.COOKIES.get('refresh')
+
+        if not refresh_token:
+            return Response({
+                'code':'empty_token',
+                'message':'No Refresh Token was provided.',
+            }, status=status.HTTP_401_UNAUTHORIZED)
+
+        data = {'refresh': refresh_token}
+        serializer = self.get_serializer(data=data)
+
         try:
-            serializeer.is_valid(raise_exception=True)
+            serializer.is_valid(raise_exception=True)
         except InvalidToken as e:
             return Response({
                 'code': "token_not_valid",
@@ -84,13 +118,29 @@ class CustomTokenRefreshView(TokenRefreshView):
             }, status=status.HTTP_401_UNAUTHORIZED)
 
         validated_data = serializer.validated_data
-        refresh_token_object = serializer.token_class(request.data['refresh'])
 
-        user= refresh_token_object.user
+        refresh_token_object = serializer.token_class(validated_data['refresh'])
+        access = refresh_token_object.access_token
+
+        user= get_user_model().objects.get(id=refresh_token_object.payload['user_id'])
 
         user_data=UserSerializer(user).data
 
-        return Response({
-            **validated_data,
-            "user":user_data
+        refresh_token=validated_data.get('refresh')
+        response= Response({
+            "user":user_data,
+            "access":str(access)
         },status=status.HTTP_200_OK)
+
+        response.set_cookie(
+            key='refresh',
+            value=refresh_token,
+            httponly=True,
+            max_age=settings.REFRESH_TOKEN_MAX_AGE,
+            secure=False,
+            samesite='None',
+            domain=None
+        )
+
+        return response
+    from rest_framework_simplejwt.serializers import TokenRefreshSerializer
