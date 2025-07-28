227e2d4bb9b6cead097e21a0caa62d98512d6fd8
Sobhan Ghosh <sobhanghosh.jsr@gmail.com>
2025-07-29 02:20:47 +0530

    feat: wrap App component with AuthProvider and FormProvider for context management

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { AuthProvider,FormProvider } from './context'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <FormProvider>
+        <App />
+      </FormProvider>
+    </AuthProvider>
   </StrictMode>,
 )
