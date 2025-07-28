a19bba99967a559f62c89b4c70795be527a057e2
Sobhan Ghosh <sobhanghosh.jsr@gmail.com>
2025-07-29 02:06:59 +0530

    feat: add LoginPage and RegisterPage exports to index.js

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -2,10 +2,14 @@ import Dashboard  from "./Dashboard";
 import Home from "./Home";
 import AllForms from "./AllForms"
 import FormBuilder from "./FormBuilder";
+import LoginPage from "./LoginPage";
+import RegisterPage from "./RegisterPage";
 
 export { 
     Home,
     Dashboard,
     AllForms,
     FormBuilder,
+    LoginPage,
+    RegisterPage
 }
